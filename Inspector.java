ca194e336af2f346059e2735a6dbb467ab1d4e1f
janlothar <janlothar@gmail.com>
2017-10-25 12:52:10 -0600

    Add getSuperclassName() and getInterfaceNames

--- Inspector.java
+++ Inspector.java
@@ -7,6 +7,8 @@ public class Inspector {
 		Class classToInspect = obj.getClass();
 		
 		getClassName(classToInspect);
+		getSuperclassName(classToInspect);
+		getInterfaceNames(classToInspect);
 		
 		if(recursive) {
 			
@@ -18,5 +20,24 @@ public class Inspector {
 		
 		String className = toInspect.getName();
 		System.out.println("Class name: " + className);
+	}
+	
+	public void getSuperclassName(Class toInspect) {
+		
+		Class superclass = toInspect.getSuperclass();
+		String superclassName = superclass.getName();
+		System.out.println("Super class name: " + superclassName);
+	}
+	
+	public void getInterfaceNames(Class toInspect) {
+		
+		Class[] interfaces = toInspect.getInterfaces();
+		String[] interfaceNames = new String[interfaces.length];
+		System.out.println("Interfaces of Class:");
+		
+		for(int i=0; i<interfaces.length; i++) {
+			interfaceNames[i] = interfaces[i].getName();
+			System.out.println("\t" + interfaceNames[i]);
+		}
 	}
 }
